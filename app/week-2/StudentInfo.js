9440d92edb556de6e08c6670b100e16a35e95b6b
camrieger <183440540+camrieger@users.noreply.github.com>
2026-01-21 18:58:43 -0700

    week 2 assignment requirements

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return(
+    <section>
+      <strong>Name: Cameron Rieger</strong>
+      <br/>
+      <Link href='https://github.com/camRieger/cprg306-assignments'>Assignment Code</Link>
+    </section>
+  )
+}
---
+++ app/week-2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from "./StudentInfo";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Week 2</h1>
+        <StudentInfo/>
+    </main>
+  );
+}
